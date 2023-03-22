4dd0f2837eba5c80063f7ec5c290c7f3ea29bf0f
ozgurcoban01 <102681848+ozgurcoban01@users.noreply.github.com>
2023-03-22 23:18:44 +0300

    module update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import { store } from "./redux/app/store";
 import { Provider } from "react-redux";
-import '@devexpress/dx-react-chart-bootstrap4/dist/dx-react-chart-bootstrap4.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
